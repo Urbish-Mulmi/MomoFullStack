4f60d0285de47164fac4fab1b7e3575f985a9bca
Urbish <Urbish Mulmi". itsformaills@gmail.com>
2026-07-02 10:23:21 +0545

    Point frontend to live backend Render established

--- frontend/src/api/apiInstance.js
+++ frontend/src/api/apiInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const api =  axios.create({
-  baseURL:"http://localhost:9000/api",
-  withCredentials:true,// this allows cookies set to be sent in each request(enable clearance pass to be in request)
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:9000/api",
+  withCredentials: true,// this allows cookies set to be sent in each request(enable clearance pass to be in request)
 });
 
 export default api;
